4e5d4826f0069b3b1701bbd03efd9d86de5a1751
Yerassyl <75775516+moooge89@users.noreply.github.com>
2022-09-04 00:24:37 +0600

    feat: started to make input component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AuthRoutingModule} from './auth-routing.module';
 import {AuthComponent} from './auth.component';
+import {InputModule} from "../../shared/input/input.module";
 
 @NgModule({
   declarations: [AuthComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
+    InputModule,
   ],
 })
 export class AuthModule {
