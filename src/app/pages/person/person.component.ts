38debf3157445b290a6225a53564f398b226a846
mooge <eraras202@mail.ru>
2023-01-08 16:47:33 +0600

    feat: added user table click handle

--- src/app/pages/person/person.component.ts
+++ src/app/pages/person/person.component.ts
@@ -1,26 +1,39 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {emptyUserFilter} from "@util/FilterUtil";
-import {MatDialog} from "@angular/material/dialog";
+import {MatDialog, MatDialogRef} from "@angular/material/dialog";
 import {PersonController} from "@controller/PersonController";
 import {Person} from "@model/person/Person";
+import {PersonDialogComponent} from "../../dialogue/person/person-dialog.component";
 
 @Component({
   selector: 'app-user',
   templateUrl: './person.component.html',
   styleUrls: ['./person.component.scss']
 })
-export class PersonComponent {
+export class PersonComponent implements OnDestroy {
 
   persons$ = this.personController.loadPersons(emptyUserFilter());
 
   panelOpenState = false;
 
+  private dialogRef: MatDialogRef<PersonDialogComponent> | undefined;
+
   constructor(private readonly dialog: MatDialog,
               private readonly personController: PersonController) {
   }
 
+  ngOnDestroy() {
+    this.dialogRef?.close();
+  }
+
   handlePersonClick(person: Person): void {
-    console.log(person);
+    this.dialogRef?.close();
+
+    this.dialogRef = this.dialog.open(PersonDialogComponent, {
+      width: '720px',
+      height: '320px',
+      data: {material: person},
+    });
   }
 
   get headers(): string[] {
