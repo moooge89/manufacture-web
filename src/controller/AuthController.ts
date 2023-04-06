23f522a9a01dd93b8040ff1dcd0ac558f6a84004
mooge <eraras202@mail.ru>
2023-04-06 21:33:21 +0600

    fix: got token.value.id

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -3,7 +3,6 @@ import {HttpService} from "@service/http/http.service";
 import {Observable} from "rxjs/internal/Observable";
 import {SecuredLoginRequest} from "@model/auth/SecuredLoginRequest";
 import {map} from "rxjs/operators";
-import {StringWrapper} from "@service/../model/wrapper/StringWrapper";
 import {BooleanWrapper} from "@model/wrapper/BooleanWrapper";
 
 @Injectable({providedIn: 'root'})
@@ -16,7 +15,7 @@ export class AuthController {
   }
 
   login(loginRequest: SecuredLoginRequest): Observable<string> {
-    return this.http.postBody<StringWrapper>('/login', loginRequest).pipe(map(x => x.value));
+    return this.http.postBody<any>('/login', loginRequest).pipe(map(x => x.value.id));
   }
 
   isValidToken(): Observable<boolean> {
