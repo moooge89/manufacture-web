b414f52b8fea98362b018acc3fd64ebee8f305a7
mooge <eraras202@mail.ru>
2023-05-09 00:20:55 +0600

    fix: redirect users to auth page if they cannot use web app

--- src/service/auth/auth.service.ts
+++ src/service/auth/auth.service.ts
@@ -5,6 +5,8 @@ import {AuthController} from "@controller/AuthController";
 import {of} from "rxjs";
 import {TOKEN_HEADER, TOKEN_PREFIX} from "@const/LocalStorageConst";
 import {UserController} from "@controller/UserController";
+import {Specialization} from "@model/user/Specialization";
+import {BottomNotificationService} from "@service/bottom-notification/bottom-notification.service";
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
@@ -15,6 +17,7 @@ export class AuthService {
     private readonly router: Router,
     private readonly authController: AuthController,
     private readonly userController: UserController,
+    private readonly notificationService: BottomNotificationService,
   ) {
   }
 
@@ -35,6 +38,25 @@ export class AuthService {
       throw new Error('IyY3pS6Qdc :: Could not successfully initialize user info');
     }
 
+    if (!this.isSpecSupported()) {
+      this.logout();
+      this.notificationService.showError('You cannot use web application');
+
+      const empty: UserInfo = {
+        id: -1,
+        specialization: Specialization.DEFAULT,
+        factory: -1,
+        department: -1,
+        firstName: '',
+        lastName: '',
+        phone: '',
+        team: -1,
+        username: '',
+      };
+
+      return of(empty).toPromise();
+    }
+
     return of(this._userInfo).toPromise();
   }
 
@@ -52,6 +74,19 @@ export class AuthService {
 
   isTokenProvided(): boolean {
     return !!localStorage.getItem(TOKEN_HEADER);
+  }
+
+  private isSpecSupported(): boolean {
+    const spec = this._userInfo?.specialization;
+
+    if (!spec) {
+      return false;
+    }
+
+    return spec === Specialization.COMPANY_DIRECTOR
+      || spec === Specialization.FACTORY_DIRECTOR
+      || spec === Specialization.DEPARTMENT_DIRECTOR
+      || spec === Specialization.SYSTEM_ADMIN;
   }
 
 }
